9cd63ae8581d652bebd0343c48196753de8a4e49
Ofek <ofek5202@gmail.com>
2024-07-27 14:27:56 +0300

    remove throw when printing all tasks and all employees

--- TaskManager.cpp
+++ TaskManager.cpp
@@ -5,10 +5,10 @@
 
 using namespace std;
 
-TaskManager::TaskManager():
-    peopleSize(0),
-    task_counter(0),
-    people(new Person*[MAX_PERSONS]{nullptr}){}
+TaskManager::TaskManager() :
+        peopleSize(0),
+        task_counter(0),
+        people(new Person *[MAX_PERSONS]{nullptr}) {}
 
 void TaskManager::assignTask(const string &personName, const Task &task) {
     if (task.getPriority() < 0 || task.getPriority() > 100) {
@@ -42,17 +42,20 @@ TaskManager::~TaskManager() {
 
 void TaskManager::bumpPriorityByType(TaskType type, int priority) {
     for (int i = 0; i < peopleSize; ++i) {
-        if(people[i] == nullptr)
+        if (people[i] == nullptr) {
             continue;
+        }
         SortedList<Task> new_tasks_list;
-        const SortedList<Task>& persons_tasks = people[i]->getTasks();
-        for (const Task& task: persons_tasks) {
-            if(task.getType() == type){
-                Task temp(task.getPriority() + priority, type, task.getDescription());
+        const SortedList<Task> &persons_tasks = people[i]->getTasks();
+        for (const Task &task: persons_tasks) {
+            if (task.getType() == type) {
+                Task temp(task.getPriority() + priority, type,
+                          task.getDescription());
                 temp.setId(task.getId());
                 new_tasks_list.insert(temp);
             } else {
-                Task temp(task.getPriority(), task.getType(), task.getDescription());
+                Task temp(task.getPriority(), task.getType(),
+                          task.getDescription());
                 temp.setId(task.getId());
                 new_tasks_list.insert(temp);
             }
@@ -68,7 +71,8 @@ void TaskManager::completeTask(const string &personName) {
                 people[i]->completeTask();
             }
             catch (const runtime_error &error) {
-                std::cerr << "Error completing task: " << error.what() << std::endl;
+                std::cerr << "Error completing task: " << error.what()
+                          << std::endl;
             }
         }
     }
@@ -76,7 +80,7 @@ void TaskManager::completeTask(const string &personName) {
 
 void TaskManager::printAllEmployees() const {
     if (peopleSize == 0) {
-        throw std::runtime_error("No employees available");
+        return;
     }
     for (int i = 0; i < peopleSize; ++i) {
         if (people[i] != nullptr) {
@@ -87,36 +91,39 @@ void TaskManager::printAllEmployees() const {
 
 void TaskManager::printAllTasks() const {
     if (peopleSize == 0) {
-        throw std::runtime_error("No tasks available");
+        return;
     }
     SortedList<Task> allTasks;
     for (int i = 0; i < peopleSize; ++i) {
-        if (!people[i])
+        if (!people[i]) {
             continue;
+        }
         const SortedList<Task> &tasks = people[i]->getTasks();
-        for (const Task& task : tasks) {
+        for (const Task &task: tasks) {
             allTasks.insert(task);
         }
     }
-    for (SortedList<Task>::ConstIterator it = allTasks.begin(); it != allTasks.end(); ++it) {
-        std::cout << *it << std::endl;
+    for (const Task &allTask: allTasks) {
+        std::cout << allTask << std::endl;
     }
 }
+
 void TaskManager::printTasksByType(TaskType type) const {
     SortedList<Task> tasks_by_type;
     for (int i = 0; i < peopleSize; ++i) {
-        if(people[i] == nullptr)
+        if (people[i] == nullptr) {
             continue;
-        const SortedList<Task>& tasks = people[i]->getTasks();
-        for(const Task& task : tasks){
-            if(task.getType() == type) {
+        }
+        const SortedList<Task> &tasks = people[i]->getTasks();
+        for (const Task &task: tasks) {
+            if (task.getType() == type) {
                 Task temp(task.getPriority(), type, task.getDescription());
                 temp.setId(task.getId());
                 tasks_by_type.insert(temp);
             }
         }
     }
-    for(const Task& task : tasks_by_type){
+    for (const Task &task: tasks_by_type) {
         std::cout << task << std::endl;
     }
 }
